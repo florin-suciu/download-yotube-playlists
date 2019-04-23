0e54513922bdfa0e47aba888aa3e40718ea7f06f
gplayersv <student.sfv@gmail.com>
2019-04-23 22:07:03 +0300

    My work here is done.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # download-yotube
-Download videos from youtube
+Download video playlists from youtube.
+
+### How to run example
+`python download_playlist.py "https://www.youtube.com/watch?v=I7zqOWF4ghQ&list=PL6s26T_VHmwHQwPQKy99xbtW81yaifzrV&index=1" "h:\Net\delme"`
---
+++ download_playlist.py
@@ -0,0 +1,18 @@
+import argparse
+
+from pytube import Playlist
+
+
+def download_playlist(playlist_url, location):
+    print(playlist_url)
+    print(location)
+    pl = Playlist(playlist_url)
+    pl.download_all(location)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("playlist_url", type=str, help="URL of playlist to download.")
+    parser.add_argument("location", type=str, help="Location where to place the downloads.")
+    args = parser.parse_args()
+    download_playlist(args.playlist_url, args.location)
