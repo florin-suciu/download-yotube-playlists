0bdce59dbde79751efec0f78856077b8c919c0f8
Florin Suciu <florin.suciu@packetfabric.com>
2022-09-27 19:14:22 +0300

    Working again.

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# download-yotube
-Download video playlists from youtube.
+# download-youtube-playlists
+Download video playlists from Youtube as `mp3`.
+
+## Installation
+
+```bash
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
 
 ### How to run example
-`python download_playlist.py "https://www.youtube.com/watch?v=I7zqOWF4ghQ&list=PL6s26T_VHmwHQwPQKy99xbtW81yaifzrV&index=1" "h:\Net\delme"`
+Just replace the URL in `download_playlist.py` and run it.
+You will get a new folder with the name of the playlist and all the videos in it in both mp4 and mp3 format.
+
+### Gotchas
+Make sure the playlist is public or unlisted, if it's private it won't work.
--- download_playlist.py
+++ download_playlist.py
@@ -1,16 +1,28 @@
-import argparse
+import os
 
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
 from pytube import Playlist
+from slugify import slugify
 
-
-def download_playlist(playlist_url, location):
-    pl = Playlist(playlist_url)
-    pl.download_all(location)
-
+PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PLalP5H2VpMn5YrAX0zJpCG9RElDJ3roQM'
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("playlist_url", type=str, help="URL of playlist to download.")
-    parser.add_argument("location", type=str, help="Location where to place the downloads.")
-    args = parser.parse_args()
-    download_playlist(args.playlist_url, args.location)
+    p = Playlist(PLAYLIST_URL)
+    print(f'Downloading playlist: {p.title}')
+    for video in p.videos:
+        audio_streams = video.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').desc()
+        print(f'Downloading: {video.title}, abr={audio_streams[0].abr}')
+        folder = slugify(p.title)
+        filename = f'{slugify(video.title)}'
+        full_path = os.path.join(folder, filename)
+        audio_streams[0].download(output_path=folder, filename=f"{filename}.mp4")
+        print(f'Downloaded: {full_path}.mp4')
+
+        mp4_file = f'{full_path}.mp4'
+        mp3_file = f'{full_path}.mp3'
+
+        print(f'Converting: {mp4_file} to {mp3_file}')
+        ffmpeg_extract_audio(mp4_file, mp3_file)
+        print(f'Converted: {mp4_file} to {mp3_file}')
+
+    print('\n')
